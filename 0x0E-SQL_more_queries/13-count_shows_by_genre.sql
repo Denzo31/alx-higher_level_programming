f4111a9199c37007e7ab1fd87bb267ac93a8999a
Denzo31 <shederrick03@gmail.com>
2024-02-14 12:19:57 -0800

    09

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
--- lists all genres from hbtn_0d_tvshows and displays the number
-of shows linked to each
+-- lists all genres from hbtn_0d_tvshows and
+displays the number of shows linked to each
 -- lists all rows of a database meeting a condition
 SELECT tv_genres.name AS 'genre',
 COUNT(tv_show_genres.genre_id) AS 'number_of_shows'
