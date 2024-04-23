cb391f97cf2d46548eec2fda0ce1931e73a3ea21
RUTAGANIRA SHEMA Derrick <shederrick03@gmail.com>
2024-04-23 21:37:42 +0200

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,7 +7,7 @@ request(process.argv[2], function (error, res, body) {
     console.log(error);
   } else {
     const result = JSON.parse(body);
-    const count = result.results.flter(x => x.characters.filter(y => y.includes('18')).length).length;
+    const count = result.results.filter(x => x.characters.filter(y => y.includes('18')).length).length;
     console.log(count);
   }
 });
